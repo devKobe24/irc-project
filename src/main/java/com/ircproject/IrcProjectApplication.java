5e751d34b40470f11b390cca1771a950d3e96643
devKobe24 <dev.skyachieve91@gmail.com>
2025-12-05 06:55:01 +0900

    first commit

---
+++ src/main/java/com/ircproject/IrcProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ircproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrcProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrcProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ircproject/IrcProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ircproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrcProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
