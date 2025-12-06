e8f69686cc2d2b9d5a9da872ddbe82dc44dedca4
devKobe24 <dev.skyachieve91@gmail.com>
2025-12-06 09:05:22 +0900

    feat(parser): IRC 형식 파싱 예외 클래스 추가
    - IrcFormatException 커스텀 예외 정의
    - RuntimeException 상속하여 비검사 예외로 구현
    - IRC 메시지 파싱 실패 시 사용할 예외 타입 제공

---
+++ src/main/java/com/ircproject/parser/IrcFormatException.java
@@ -0,0 +1,18 @@
+package com.ircproject.parser;
+
+/**
+ * packageName    : com.ircproject.parser
+ * fileName       : IrcFormatException
+ * author         : kobe
+ * date           : 2025. 12. 5.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2025. 12. 5.        kobe       최초 생성
+ */
+public class IrcFormatException extends RuntimeException {
+    public IrcFormatException(String message) {
+        super(message);
+    }
+}
